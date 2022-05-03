33f5234e85d35489bb5adfeaf4b947f7a959a523
Melquesedek Antonio de Oliveira <melquesedekdeoliveira@gmail.com>
2022-05-03 07:18:09 -0300

    Curso Python Básico FIM

---
+++ Introdução/final.py
@@ -0,0 +1,3 @@
+#As últimas aulas eram basicamente práticas do que já foi aprendido, então não irei postar aqui.
+#Eram basicamente realizar consultas por webservices (API) e também postagens.
+#E por fim, a criação de uma biblioteca, que era um arquivo que pode ter várias classes/metodos. Então não é algo tão diferente do que já aprendemos.
